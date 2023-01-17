523e81cd95ab7973474daf6950ab0d5d3176cd30
kimseungwoo <aoome123@naver.com>
2023-01-17 18:54:55 +0900

    study

---
+++ inch_to_cm.py
@@ -0,0 +1,8 @@
+# inch를 cm로 바꾸기
+
+inch_input = input("inch 단위의 숫자를 입력해주세요 >")
+
+inch = int(inch_input)
+cm = 2.54 * inch
+
+print(inch, "inch는 cm 단위로", cm, "cm 입니다.")
---
+++ input.py
@@ -0,0 +1,9 @@
+# 입력 자료형 확인
+# input 함수는 무조건 문자열이 나온다.
+
+string = input("입력>")
+
+# 출력
+
+print("자료:", string)
+print("자료형:", type(string))
