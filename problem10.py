bb97baa3d85f0144692b13f97fa7746c3288c0e8
kimseungwoo <aoome123@naver.com>
2023-01-20 16:52:56 +0900

    study

---
+++ problem10.py
@@ -0,0 +1,8 @@
+# 숫자는 무작위로 입력해도 됨
+key_list=["name","hp","mp","level"]
+value_list=["기사",200,30,5]
+character = {}
+
+for i in range(0,len(key_list)):
+    character[key_list[i]] = value_list[i]
+print(character)
